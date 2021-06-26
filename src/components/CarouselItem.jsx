ddf269657ace108fb0a7de5b26a73cf849c41901
isabelajs <isabelajimenezs96@gmail.com>
2021-06-26 18:54:52 -0500

    utilice proptype

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,20 +1,34 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import '../assets/styles/components/CarouselItem.scss' ;
 
-function CarouselItem() {
+function CarouselItem({cover, title, year, contentRating, duration}) {
   return (
     <div className='carousel-item'>
-      <img className='carousel-item__img' src='https://www.fundacion-affinity.org/sites/default/files/el-gato-necesita-tener-acceso-al-exterior.jpg' alt='' />
+      <img className='carousel-item__img' src={cover} alt={title} />
       <div className='carousel-item__details'>
         <div className='icons'>
           <div className='icons__icon icons__icon--play' />
           <div className='icons__icon icons__icon--plus' />
         </div>
-        <p className='title'>Título descriptivo</p>
-        <p className='subtitle'>2019 16+ 114 minutos</p>
+        <p className='title'>{title}</p>
+        <p className='subtitle'>
+          {`${year} ${contentRating} ${duration}`}
+        </p>
       </div>
     </div>
   );
 }
 
+
+// se define como se reciben los datos del componente, me notifica si no se cumple la condición
+CarouselItem.propTypes = {
+  cover: PropTypes.string,
+  title: PropTypes.string,
+  contentRating: PropTypes.string,
+  duration: PropTypes.number,
+  year: PropTypes.number,
+
+
+}
 export default CarouselItem;
--- src/container/App.js
+++ src/container/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+
+import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Main from '../components/Main';
@@ -8,8 +9,13 @@ import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
 import Footer from '../components/Footer';
 import '../assets/styles/App.scss';
+import data from '../../initialState.json'
+import { initial } from 'lodash';
 
 function App() {
+  const {initalState} = data
+
+
   return (
     <div className='App'>
       <Header />
@@ -19,29 +25,35 @@ function App() {
 
         <Categories>
 
-          <Categorie title='Gaticos'>
+
+      {/* //si dentro del componente lista existen elementos */}
+        {
+          initalState.mylist.length > 0 &&
+          <Categorie title= 'mi lista'>
             <Carousel>
-              <CarouselItem />
-              <CarouselItem />
-              <CarouselItem />
-              <CarouselItem />
-              <CarouselItem />
-              <CarouselItem />
-              <CarouselItem />
-              <CarouselItem />
-              <CarouselItem />
-              <CarouselItem />
-              <CarouselItem />
-              <CarouselItem />
               <CarouselItem />
             </Carousel>
           </Categorie>
+        }
 
-          <Categorie title='Perritos'>
+        
+          <Categorie title='Tendencias'>
             <Carousel>
-              <CarouselItem />
-              <CarouselItem />
-              <CarouselItem />
+            { initalState.trends.length > 0 &&
+              initalState.trends.map(video =>
+                <CarouselItem key={video.id} {...video}/>
+                )
+            }
+            </Carousel>
+          </Categorie>
+
+          <Categorie title='Originales'>
+            <Carousel>
+            { initalState.originals.length > 0 &&
+              initalState.originals.map(video =>
+                <CarouselItem key={video.id} {...video}/>
+                )
+            }
             </Carousel>
           </Categorie>
 
